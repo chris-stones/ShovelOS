c8ae31a4433b954ac81be3f7c5a966332d386676
Chris Stones <chris.stones@zoho.com>
2014-03-11 22:55:44 +0000

    Added code for generating VMSAv6 supersections.

---
+++ src/arch/arm/stdint.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+typedef                    char   int16_t;
+typedef unsigned           char  uint16_t;
+typedef                    short  int16_t;
+typedef unsigned           short uint16_t;
+typedef                    int    int32_t;
+typedef unsigned           int   uint32_t;
+typedef          long long int    int64_t;
+typedef unsigned long long int   uint64_t;
+
+typedef unsigned           int phy_addr32_t;
+typedef unsigned long long int phy_addr40_t;
+
+typedef void*        		phy_ptr_t;
