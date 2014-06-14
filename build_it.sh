46d8882322193ad3fe9bc6a32736ee1584449dd5
Chris Stones <chris.stones@zoho.com>
2014-06-14 17:44:25 +0100

    clean and build script.

---
+++ build_it.sh
@@ -0,0 +1,3 @@
+make LD_FLAGS="-L /usr/lib64/gcc/armv7a-none-eabi/4.8.2/" ARCH="arm" VMSA="VMSAv7" CROSS_COMPILE="armv7a-none-eabi-" -C src/ clean
+make LD_FLAGS="-L /usr/lib64/gcc/armv7a-none-eabi/4.8.2/" ARCH="arm" VMSA="VMSAv7" CROSS_COMPILE="armv7a-none-eabi-" -C src/ 
+
