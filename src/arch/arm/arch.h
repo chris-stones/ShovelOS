11ad1ff12059f3438e6ee86ec11b36b05de00428
Chris Stones <chris.stones@zoho.com>
2015-06-02 12:55:07 +0100

    Added _BREAK macro for debugging.

--- src/arch/arm/arch.h
+++ src/arch/arm/arch.h
@@ -1,10 +1,17 @@
 
 #pragma once
 
+#include<stdint.h>
+
 ssize_t _debug_out( const char * string );
 ssize_t _debug_out_uint( uint32_t i );
+void _break(const char * file, const char * func, int line);
+
+#define _BREAK() _break(__FILE__,__FUNCTION__,__LINE__)
 
 void dcache_clean();
 void dcache_invalidate();
 void dcache_clean_invalidate();
 void icache_invalidate();
+
+void * __debug_pt_root;
