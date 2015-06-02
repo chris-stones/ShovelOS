48b413cdd2d4e2ab4aea0e535f9f730abcfb342b
Chris Stones <chris.stones@zoho.com>
2015-06-02 12:53:09 +0100

    New compiler version.

--- build_it.sh
+++ build_it.sh
@@ -1,2 +1,2 @@
-make LD_FLAGS="-L /usr/lib64/gcc/arm-none-eabi/4.9.2/" ARCH="arm" VMSA="VMSAv7" CROSS_COMPILE="arm-none-eabi-" -C src/ clean
-make LD_FLAGS="-L /usr/lib64/gcc/arm-none-eabi/4.9.2/" ARCH="arm" VMSA="VMSAv7" CROSS_COMPILE="arm-none-eabi-" -C src/ 
+make ARCH="arm" VMSA="VMSAv7" -C src/ clean
+make LD_FLAGS="-L /usr/lib64/gcc/arm-none-eabi/5.1.0/" ARCH="arm" VMSA="VMSAv7" CROSS_COMPILE="arm-none-eabi-" -C src/ 
