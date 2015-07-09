3f122e05d0d80880a6a951b0ea737678286c3ab3
Chris Stones <chris.stones@zoho.com>
2015-07-09 13:03:00 +0100

    Added a few system control flags.

--- src/arch/arm/system_control_register.h
+++ src/arch/arm/system_control_register.h
@@ -6,8 +6,12 @@
 typedef enum system_control_register {
 
 	SCTLR_TE 							= (1<<30),	// Thumb exception enable.
+	SCTLR_AFE							= (1<<29),  // Access flag enable.
 	SCTLR_TRE							= (1<<28),	// TEX re-map enable.
+	SCTLR_NMFE                          = (1<<27),  // Non-maskable FIQ Enable.
+	SCTLR_EE                            = (1<<25),  // Exception Endianness.
 	SCTLR_VE							= (1<<24),  // DEPRECATED. Vector interrupt enable. 0=see 'V', 1=implementation defined.
+	SCTLR_FI                            = (1<<21),  // Fast interrupt configuration enable.
 	SCTLR_V								= (1<<13),  // V exception vector base address, 0=0x00000000 ( SEE VBAR ),1=0xffff0000.
 	SCTLR_I								= (1<<12),	// Instruction cache enable.
 	SCTLR_Z 							= (1<<11),	// Branch prediction enable.
