b2ecb3f0ef092280a1c2a31de178d667410d0019
chris-stones <chris.stones@zoho.com>
2016-08-24 13:52:50 +0100

    more cache bug fixes!

--- src/arch/arm/mm/VMSAv7/cache.c
+++ src/arch/arm/mm/VMSAv7/cache.c
@@ -22,7 +22,7 @@ static uint32_t _log2_roundup(uint32_t l) {
 static uint32_t encode_set_way(
 		uint32_t cache_level,
 		uint32_t associativity,
-		uint32_t linelength,
+		uint32_t lineword,
 		uint32_t set,
 		uint32_t way)
 {
@@ -48,16 +48,13 @@ static uint32_t encode_set_way(
 	// A = Log2(ASSOCIATIVITY), rounded up to the next integer if necessary.
 	uint32_t A = _log2_roundup(associativity);
 
-	// L = Log2(LINELENGTH)
-	uint32_t L = _log2(linelength);
-
 	// Level = (Cache level to operate on)-1
-	uint32_t Level = cache_level /* -1 */;
+	uint32_t Level = cache_level;
 
 	uint32_t reg =
-			(way 	<< (32-A))	|
-			(set 	<< 		L)	|
-			(Level 	<< 		1)	;
+			(way 	<<        (32-A)) |
+			(set 	<< 	lineword) |
+			(Level 	<< 	       1) ;
 
 //	_debug_out("    A           "); _debug_out_uint(A); _debug_out("\r\n");
 //	_debug_out("    L           "); _debug_out_uint(L); _debug_out("\r\n");
@@ -81,11 +78,9 @@ static void dcache_do_level(enum dcache_op op, uint32_t level ) {
 
 	uint32_t ccsidr = _arm_cp_read_CCSIDR();
 
-	uint32_t nsets 			=      (1 + ((ccsidr & 0x0FFFE000) >> 13));
+	uint32_t nsets 		=      (1 + ((ccsidr & 0x0FFFE000) >> 13));
 	uint32_t associativity	=      (1 + ((ccsidr & 0x00001FF8) >>  3));
-	uint32_t linewords_raw  =           ((ccsidr & 0x00000007)       );
-	uint32_t linewords		= 1 << (2 + ((linewords_raw      )      ));
-	uint32_t linebytes		= 4 * linewords;
+	uint32_t lineword       =      (4 + ((ccsidr & 0x00000007) >>  0));
 
 //	_debug_out("level      "); _debug_out_uint(level); _debug_out("\r\n");
 //	_debug_out("sets       "); _debug_out_uint(nsets);   _debug_out("\r\n");
@@ -99,7 +94,7 @@ static void dcache_do_level(enum dcache_op op, uint32_t level ) {
 		for(uint32_t set = 0; set < nsets; set++) {
 
 			uint32_t setway =
-				encode_set_way(level,associativity,linebytes,set,way);
+				encode_set_way(level,associativity,lineword,set,way);
 
 			switch(op) {
 			case DCACHE_CLEAN:
