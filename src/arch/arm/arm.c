1f1e57b4e0172eda76a26a2a51bbbcf84c7f2278
Chris Stones <mail@chris-stones.uk>
2016-08-30 13:33:24 +0100

    write BREAK macro hits to console.

--- src/arch/arm/arm.c
+++ src/arch/arm/arm.c
@@ -10,10 +10,10 @@
 #include "mm/VMSAv7/small_page.h"
 
 void _break(const char * file, const char * func, int line) {
-//	_debug_out(">>>BREAK!\r\n");
-//	_debug_out(file);_debug_out("\r\n");
-//	_debug_out(func);_debug_out("\r\n");
-//	_debug_out_uint(line);_debug_out("\r\n");
-//	_debug_out("<<<BREAK!\r\n");
+	_debug_out(">>>BREAK!\r\n");
+	_debug_out(file);_debug_out("\r\n");
+	_debug_out(func);_debug_out("\r\n");
+	_debug_out_uint(line);_debug_out("\r\n");
+	_debug_out("<<<BREAK!\r\n");
 	return ; // just a point to attach a debugger to.
 }
