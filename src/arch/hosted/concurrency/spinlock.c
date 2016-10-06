4c79059dca6093cd8390b8dcf14f2dbc74fec833
Chris Stones <chris.stones@gmail.com>
2016-10-06 14:09:04 +0100

    use libNix / libWin32 for threading / locking on hosted builds.

--- src/arch/hosted/concurrency/spinlock.c
+++ src/arch/hosted/concurrency/spinlock.c
@@ -3,15 +3,36 @@
 
 #include "spinlock.h"
 
+#if defined(_MSC_VER)
+	#include<libWin32/libWin32.h>
+#endif
+
+void spinlock_init(spinlock_t * lock) {
+
+	mutex_init(&lock->mutex);
+}
+
+void spinlock_destroy(spinlock_t * lock) {
+
+	mutex_destroy(&lock->mutex);
+}
+
 void spinlock_lock_irqsave(spinlock_t * lock) {
-	// TODO:
+
+	mutex_lock(&lock->mutex);
 }
+
 void spinlock_unlock_irqrestore(spinlock_t * lock) {
-	// TODO:
+
+	mutex_unlock(&lock->mutex);
 }
+
 void spinlock_lock(spinlock_t * lock) {
-	// TODO:
+
+	mutex_lock(&lock->mutex);
 }
+
 void spinlock_unlock(spinlock_t * lock) {
-	// TODO:
+
+	mutex_unlock(&lock->mutex);
 }
