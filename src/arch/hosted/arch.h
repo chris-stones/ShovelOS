d9ebe8f90a45005f1f5c5c445023536acfb6683f
Chris Stones <mail@chris-stones.uk>
2016-10-15 06:22:30 +0100

    implement _BUG_ON for hosted builds.

--- src/arch/hosted/arch.h
+++ src/arch/hosted/arch.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include<stdint.h>
+
 static inline void _zero_bss() {
 	// do nothing - 
 	//	the host OS already cleared our BSS section for us.
@@ -9,7 +11,12 @@ static inline void exceptions_setup() {
 
 }
 
+ssize_t _debug_out( const char * string );
+ssize_t _debug_out_uint( uint32_t i );
+
 int in_interrupt();
 void register_drivers();
+
+void _bug(const char * file, const char * func, int line);
 
 #define _BUG_ON(x) do{if((x)) _bug(__FILE__,__FUNCTION__,__LINE__);}while(0)
