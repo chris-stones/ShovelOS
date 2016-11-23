fb33d2b43dd95e63ea92fb0f40774f9617d3d58d
cds84 <email@chris-stones.uk>
2016-11-23 12:50:35 +0000

    Check for presence of CPSR in cpu_state.h

--- src/arch/arm/cpu_state.h
+++ src/arch/arm/cpu_state.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <stdint.h>
+#include <cpu_caps.h>
 
 // NOTE - THIS STRUCT COUPLED TIGHTLY WITH _my_IRQ_handler in context.S
 // cpu_state must be first
 struct cpu_state_struct {
+#if defined(__CONFIG_ARM_CPSR__) 
 	uint32_t CPSR;
+#endif 
 	uint32_t PC;
 	uint32_t SP;
 	uint32_t LR;
