8ab864c5b3513484342bdb016bb0bfccf4a22899
cds84 <email@chris-stones.uk>
2016-11-23 17:03:58 +0000

    add cpu_caps checks for profile and CLREX

--- src/arch/arm/cpu_caps.h
+++ src/arch/arm/cpu_caps.h
@@ -6,18 +6,24 @@
   #define __CONFIG_ARM_CPSR__
   #define __CONFIG_ARM_NO_xPSR__
   #define __CONFIG_ARM_NO_PRIMASK__
+  #define __CONFIG_ARM_CLREX__
+  #define __CONFIG_ARM_A__
 #endif
 #if defined(__ARM_ARCH_7A__)
   #define __CONFIG_ARM_CP__
   #define __CONFIG_ARM_CPSR__
   #define __CONFIG_ARM_NO_xPSR__
   #define __CONFIG_ARM_NO_PRIMASK__
+  #define __CONFIG_ARM_CLREX__
+  #define __CONFIG_ARM_A__
 #endif
 #if defined(__ARM_ARCH_6M__)
   #define __CONFIG_ARM_NO_CP__
   #define __CONFIG_ARM_NO_CPSR__
   #define __CONFIG_ARM_xPSR__
   #define __CONFIG_ARM_PRIMASK__
+  #define __CONFIG_ARM_NO_CLREX__
+  #define __CONFIG_ARM_M__
 #endif
 
 #if !defined(__CONFIG_ARM_CP__) && !defined(__CONFIG_ARM_NO_CP__)
@@ -29,9 +35,18 @@
 #endif
 
 #if !defined(__CONFIG_ARM_xPSR__) && !defined(__CONFIG_ARM_NO_xPSR__)
-  #error Cant determine if ARM CPU has a special register CPSR
+  #error Cant determine if ARM CPU has a special register xPSR
 #endif
 
 #if !defined(__CONFIG_ARM_PRIMASK__) && !defined(__CONFIG_ARM_NO_PRIMASK__)
-  #error Cant determine if ARM CPU has a special register CPSR
+  #error Cant determine if ARM CPU has a special register PRIMASK
 #endif
+
+#if !defined(__CONFIG_ARM_CLREX__) && !defined(__CONFIG_ARM_NO_CLREX__)
+  #error Cant determine if ARM CPU has clrex family of instructions.
+#endif
+
+#if !defined(__CONFIG_ARM_A__) && !defined(__CONFIG_ARM_M__)
+#error Cant determine if ARM CPU profile (A/M)
+#endif
+
