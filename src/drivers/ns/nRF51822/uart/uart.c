2e66369a149e1a18e9c77d3b2d993dc0cfd95bc4
cds84 <email@chris-stones.uk>
2016-12-03 20:35:09 +0000

    test UART

--- src/drivers/ns/nRF51822/uart/uart.c
+++ src/drivers/ns/nRF51822/uart/uart.c
@@ -2,6 +2,21 @@
 #include <_config.h>
 #include <stdint.h>
 
+#include "regs.h"
+
+//static
+void bbc_microbit_setup_uart() {
+
+  BAUDRATE = 0x004EA000; // 19200
+  PSELRXD  = 25; // pin select RXD.
+  PSELTXD  = 24; // pin select TXD.
+  ENABLE   = 4;  // enable UART
+  STARTTX  = 1;  // START TX
+
+  for(;;)
+    TXD = (unsigned int)'A';
+}
+
 ssize_t _debug_out( const char * string ) {
   
   return 0;
