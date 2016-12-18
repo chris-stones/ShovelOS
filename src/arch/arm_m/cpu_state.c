f0a6bda9c312617f3737047b25b81d6e7c7390db
cds84 <email@chris-stones.uk>
2016-12-18 20:49:45 +0000

    arm-m task switching ISR.

--- src/arch/arm_m/cpu_state.c
+++ src/arch/arm_m/cpu_state.c
@@ -2,17 +2,39 @@
 #include <_config.h>
 #include <cpu_state.h>
 
+// NOTE - This is an exception frame.
+//  It is defined in the ARMv6-m Architecture reference manual - dont modify!
+//  It MUST be ligned on an 8-byte boundary.
+//  the arm-m cpu pushes theese registers onto the stack automatically on exception.
+struct exception_frame {
+  uint32_t R0;
+  uint32_t R1;
+  uint32_t R2;
+  uint32_t R3;
+  uint32_t R12;
+  uint32_t LR;
+  uint32_t PC;
+  uint32_t xPSR_fpalign; // bit-9 is a alignment flag (reserved in xPSR)
+};
+
 int cpu_state_build(struct cpu_state_struct * cpuss,
 		    void *(*start)(void*),
 		    void *args,
 		    void *stack,
 		    void  (*end)())
 {
-  cpuss->PC = (uint32_t)(start);
-  cpuss->LR = (uint32_t)(end);
-  cpuss->R0 = (uint32_t)(args);
-
-  cpuss->xPSR_fpalign =
+  // Set stack pointer with space allocated for exception frame and other state.
+  // SEE context.S
+  cpuss->SP = (size_t)stack - (sizeof cpuss->exception_frame + 36);
+ 
+  // Create an exception frame.
+  struct exception_frame *ef =
+    (struct exception_frame*)cpuss->SP;
+  
+  ef->PC = (uint32_t)(start);
+  ef->LR = (uint32_t)(end);
+  ef->R0 = (uint32_t)(args);
+  ef->.xPSR_fpalign =
     1<<24; // EPSR - Thumb-mode bit.
   
   return 0;
--- src/arch/arm_m/cpu_state.h
+++ src/arch/arm_m/cpu_state.h
@@ -2,36 +2,11 @@
 
 #include<stdint.h>
 
-// NOTE - This is an exception frame.
-//  It is defined in the ARMv6-m Architecture reference manual - dont modify!
-//  It MUST be ligned on an 8-byte boundary.
-//  the arm-m cpu pushes theese registers onto the stack automatically on exception.
-struct exception_frame {
-  uint32_t R0;
-  uint32_t R1;
-  uint32_t R2;
-  uint32_t R3;
-  uint32_t R12;
-  uint32_t LR;
-  uint32_t PC;
-  uint32_t xPSR_fpalign; // bit-9 is a alignment flag (reserved in xPSR)
-};
-
+// NOTE - All CPU state is stored on the stack.
+//   Scheduler need only store the stack pointer.
 struct cpu_state_struct {
 
-  // registers manually pushed onto the stack by the task-switch interrupt handler.
-  uint32_t R4;
-  uint32_t R5;
-  uint32_t R6;
-  uint32_t R7;
-  uint32_t R8;
-  uint32_t R9;
-  uint32_r R10;
-  uint32_t R11;
   uint32_t SP;
-
-  // registers puhed onto the stack by the CPU on exception.
-  struct exception_frame exception_frame;
 };
 
 int cpu_state_build(struct cpu_state_struct * cpuss,
