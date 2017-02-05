5afd6579fec34b04f8a8af81460dd47ec5632ecc
Chris Stones <mail@chris-stones.uk>
2017-02-05 20:05:36 +0000

    rename cps id/restore

--- src/arch/arm_m/special/primask.h
+++ src/arch/arm_m/special/primask.h
@@ -9,12 +9,14 @@ _ARM_DEFINE_SPECIAL_REG(primask)
 static inline uint32_t armm_cpsid() {
 
   uint32_t primask = _arm_primask_read();
-  
-  __asm__ __volatile__ ("cpsid i");
-  dsb();
-  isb();
 
-  return primask & 1;
+  if((primask & 1)==0) {
+    __asm__ __volatile__ ("cpsid i");
+    dsb();
+    isb();
+    return 0;
+  }
+  return 1;
 }
 
 static inline uint32_t armm_cpsie() {
@@ -26,7 +28,7 @@ static inline uint32_t armm_cpsie() {
   return primask & 1;
 }
 
-static inline void _arm_cps_restore(uint32_t flags) {
+static inline void armm_cps_restore(uint32_t flags) {
 
   if(flags & 1) {
      __asm__ __volatile__ ("cpsid i");
