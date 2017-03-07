917d916628a3b21ab918c44bbf81ad00c14871e4
cds84 <email@chris-stones.uk>
2017-03-07 10:28:07 +0000

    Added size and cancel functionality.

--- src/memory/fifo_packet_buffer.h
+++ src/memory/fifo_packet_buffer.h
@@ -7,6 +7,7 @@
 
 struct packet_fifo {
   uint8_t ** bp; // base
+  uint8_t  flags;// locked?
   uint16_t ri;   // read index
   uint16_t wi;   // write index
   uint16_t sz;   // buffer size
@@ -18,8 +19,12 @@ struct packet_fifo {
 int  packet_fifo_create (struct packet_fifo * buffer, size_t packet_size, size_t packets);
 void packet_fifo_destroy(struct packet_fifo * buffer);
 
+int packet_fifo_size         (struct packet_fifo * buffer);
+
 int packet_fifo_write_lock   (struct packet_fifo * buffer, uint8_t ** const packet);
-int packet_fifo_write_release(struct packet_fifo * buffer, uint8_t  * const packet);
+int packet_fifo_write_cancel (struct packet_fifo * buffer, uint8_t *  const packet);
+int packet_fifo_write_release(struct packet_fifo * buffer, uint8_t *  const packet);
 
 int packet_fifo_read_lock   (struct packet_fifo * buffer, const uint8_t ** const packet);
-int packet_fifo_read_release(struct packet_fifo * buffer, const  uint8_t *  const packet);
+int packet_fifo_read_cancel (struct packet_fifo * buffer, const uint8_t *  const packet);
+int packet_fifo_read_release(struct packet_fifo * buffer, const uint8_t *  const packet);
