ec12634c7a32b121c630812d926a2a9e3f870723
Chris Stones <email@chris-stones.uk>
2017-08-16 20:36:58 +0100

    install kernel as stage-1.5 boot-loader.

--- utils/test_i386.sh
+++ utils/test_i386.sh
@@ -3,7 +3,7 @@
 ./utils/make_pc_bios_disk.sh ./images/i386-pc.img
 ./utils/install.sh $1/src/bootloader/i386-pc/mbr ./images/mbr.bin
 ./utils/install.sh $1/src/bootloader/i386-pc/vbr ./images/vbr.bin
-./utils/install.sh $1/src/bootloader/i386-pc/stage1.5/stage1_5 ./images/stage1.5.bin
+./utils/install.sh $1/src/kernel/kernel ./images/stage1.5.bin
 ./utils/install_mbr.sh ./images/mbr.bin ./images/i386-pc.img
 ./utils/install_vbr.sh ./images/vbr.bin ./images/i386-pc.img 2048
 ./utils/install_stage1.5.sh ./images/stage1.5.bin ./images/i386-pc.img
